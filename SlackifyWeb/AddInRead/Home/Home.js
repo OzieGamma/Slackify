b507b4c5e24a50bc52f7acff890db3f2d05a3140
Oswald Maskens <ozie@live.be>
2016-02-26 11:29:41 +0100

    Works with simple emails

--- SlackifyWeb/AddInRead/Home/Home.js
+++ SlackifyWeb/AddInRead/Home/Home.js
@@ -1,30 +1,75 @@
 ﻿/// <reference path="../App.js" />
 
-(function () {
+(function() {
     "use strict";
 
     // The Office initialize function must be run each time a new page is loaded
-    Office.initialize = function (reason) {
-        $(document).ready(function () {
+    Office.initialize = function(reason) {
+        $(document).ready(function() {
             app.initialize();
 
             displayItemDetails();
-                       
+
 
         });
     };
 
     // Displays the "Subject" and "From" fields, based on the current mail item
     function displayItemDetails() {
-        var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
-        Office.context.mailbox.item.body.getAsync("html", {}, function (results) {
-                      
-          
-            
+        function extractDateTime(headers) {
+            if (headers.length <= 1) {
+                return null;
+            }
 
-            $('#subject').text(results.value);
-                       
-            
+            var dateTimeRegEx = /<b>(EnvoyÃ©&nbsp;|Sent):<\/b> ([\s\S]+)/;
+            var dateTimeMatch = dateTimeRegEx.exec(headers[1].trim());
+
+            return (dateTimeMatch === null) ? null : dateTimeMatch[2];
+        };
+
+        function extractFrom(headers) {
+            if (headers.length === 0) {
+                return null;
+            }
+
+            var fromRegEx = /([a-zA-Z0-9\s]*) \[<a href=\"mailto:([\s\S]+@[\s\S]+)\">mailto:[\s\S]+@[\s\S]+<\/a>\]/;
+            var fromMatch = fromRegEx.exec(headers[0].trim());
+
+            return (fromMatch === null) ? null : {
+                displayName: fromMatch[1],
+                emailAddress: fromMatch[2]
+            };
+        }
+
+        var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
+        Office.context.mailbox.item.body.getAsync("html", {}, function(results) {
+            var fakeDom = $(document.createElement("html"));
+            fakeDom.html(results.value);
+            var conversation = $("div.WordSection1", fakeDom).html();
+            var text = conversation.replace(/<p class="MsoNormal">/g, "")
+                .replace(/<\/p>/g, "\n")
+                .replace(/<span\b[^>]*>/g, "")
+                .replace(/<\/span>/g, "")
+                .split(/<b>De&nbsp;:<\/b>|<b>From:<\/b>/);
+
+            var messages = text.map(function(el) {
+                var msgBegin = el.split("Sincerely,")[0];
+                var splitedMsgBegin = msgBegin.split(/Hi\s[a-zA-Z0-9]*,/g);
+                var msgHeaders = splitedMsgBegin[0].split("<br>");
+                var msgBody = splitedMsgBegin[1].replace(/&nbsp;/g, " ").trim();
+
+                return {
+                    from: extractFrom(msgHeaders),
+                    dateTime: extractDateTime(msgHeaders),
+                    body: msgBody
+                };
+            });
+
+            messages[0].from = Office.context.mailbox.item.from;
+            messages[0].dateTime = Office.context.mailbox.item.dateTimeCreated.toString();
+
+            $("#subject").text(messages.map(JSON.stringify));
+
         });
 
         var from;
@@ -35,10 +80,11 @@
         }
 
         if (from) {
-            $('#from').text(from.displayName);
-            $('#from').click(function () {
+            $("#from").text(from.displayName);
+            $("#from").click(function() {
                 app.showNotification(from.displayName, from.emailAddress);
             });
         }
     }
-})();
+})
+();
